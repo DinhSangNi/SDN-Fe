c393ba41dcf66c12202fdbeffd950e295956bdbf
Vang Huynh <vangpro1991@gmail.com>
2025-02-16 07:55:52 +0700

    deploy profile

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,4 +1,12 @@
 import Image from 'next/image';
+
+const PROFILES = [
+  {
+    image: '/avatars/huynhvang.jpg',
+    name: 'Huỳnh Vang',
+    description: 'Giúp đỡ các bạn sinh viên hoàn thành các công việc code',
+  },
+];
 const HomePage = async () => {
   return (
     <section className=''>
@@ -6,29 +14,33 @@ const HomePage = async () => {
         Các chuyên gia của chúng tôi
       </h3>
 
-      <div className='flex flex-col items-center md:flex-row mt-2'>
-        <article className='group relative'>
-          <div className='relative h-[500px] mb-2 overflow-hidden rounded-md'>
-            <Image
-              src={'/avatars/huynhvang.jpg'}
-              fill
-              sizes='(max-width: 768px) 100vw, 50vw'
-              alt={'Huynh Vang'}
-              className='rounded-md object-cover transform group-hover:scale-110 transition-transform duration-500'
-            />
-          </div>
-          <div className='flex justify-between items-center'>
-            <h3 className='text-sm font-semibold mt-1 px-2'>{'Huỳnh Vang'}</h3>
-          </div>
-          <p className='text-sm mt-1 font-semibold px-2'>{'Giảng viên IT'}</p>
-          <div className='flex justify-between items-center mt-1 mb-2 px-2'>
-            <p className='text-sm mt-1'>
-              <span className='text-muted-foreground px-2'>
-                {'Giúp đỡ các bạn sinh viên hoàn thành các công việc code'}{' '}
-              </span>
-            </p>
-          </div>
-        </article>
+      <div className='flex flex-col items-center md:flex-row mt-2 gap-3'>
+        {PROFILES.map((profile) => (
+          <article key={profile.name} className='group relative'>
+            <div className='relative h-[500px] mb-2 overflow-hidden rounded-md'>
+              <Image
+                src={profile.image}
+                fill
+                sizes='(max-width: 768px) 100vw, 50vw'
+                alt={profile.name}
+                className='rounded-md object-cover transform group-hover:scale-110 transition-transform duration-500'
+              />
+            </div>
+            <div className='flex justify-between items-center'>
+              <h3 className='text-sm font-semibold mt-1 px-2'>
+                {profile.name}
+              </h3>
+            </div>
+            <p className='text-sm mt-1 font-semibold px-2'>{'Giảng viên IT'}</p>
+            <div className='flex justify-between items-center mt-1 mb-2 px-2'>
+              <p className='text-sm mt-1'>
+                <span className='text-muted-foreground px-2'>
+                  {profile.description}
+                </span>
+              </p>
+            </div>
+          </article>
+        ))}
       </div>
     </section>
   );
